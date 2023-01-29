42fd46cc79a5de3fce48af7c427f767b3f78f634
John Abou <abounassar.john@gmail.com>
2023-01-29 10:34:03 -0500

    Added image icons to be used in the current weather card. Added a function that makes a request to get the forecast weather. (currently there is a problem with the request -- 401 response).

--- Develop/index.js
+++ Develop/index.js
@@ -1,3 +1,4 @@
+// Define all variables
 // Define the global variables
 let APIkey = '5d82752b5eec77e02284baee59150776';
 let searchHistory = [];
@@ -5,6 +6,21 @@ let searchHistory = [];
 // Define the DOM variables
 let currentWeatherElement = document.querySelector('#current-weather');
 let asideElement = document.querySelector('aside');
+
+
+// Define all the variables
+//Create a function to make the iso date time to a human readable timestamp.
+function isoToReadableDate( isoNumber ) {
+    let milliseconds = isoNumber * 1000;
+    let dateObject = new Date(milliseconds);
+    humanDateFormat = dateObject.toLocaleString();
+    return humanDateFormat;
+}
+
+// Create a function to run on the start of the page
+function init() {
+    getCoordinates("Toronto");
+}
 
 // Create a function to save the search history to local storage
 function saveSearchHistory() {
@@ -41,27 +57,22 @@ function getCoordinates( cityName ) {
             // Get the latitude and longitude from the response.
             let latitude = data[0]['lat'];
             let longitude = data[0]['lon'];
+            currentWeatherElement.children[0].innerHTML = cityName;
             currentWeatherElement.setAttribute('data-lat', latitude);
             currentWeatherElement.setAttribute('data-lon', longitude);
 
 
             // After the city has been retrieved, get the current weather and future weather
             getCurrentWeather();
+            getForecastWeather();
         })        
-}
-
-function isoToReadableDate( isoNumber ) {
-    let milliseconds = isoNumber * 1000;
-    let dateObject = new Date(milliseconds);
-    humanDateFormat = dateObject.toLocaleString();
-    return humanDateFormat;
 }
 
 //Create a function to get the current weather from the OpenWeather API
 function getCurrentWeather () {
     let lat = currentWeatherElement.getAttribute('data-lat');
     let long = currentWeatherElement.getAttribute('data-lon');
-    let queryString = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + "&appid="+ APIkey;
+    let queryString = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + "&appid=" + APIkey;
     
     fetch(queryString)
         .then(function (response) {
@@ -72,18 +83,42 @@ function getCurrentWeather () {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            //the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-            console.log(data.main["temp"]);
-            console.log(data.weather[0]["icon"]); // ---> this does not produce an icon
-            // Fill the src attr for the icon with the string: https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon
-            console.log(data.main["humidity"]);
-            console.log(data.wind["speed"])
+            //Grab the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
+            let temp = Math.floor(data.main['temp'] - 275.15);
+            let iconCode = data.weather[0]["icon"];
+            let humidity = data.main["humidity"];
+            let windSpeed = data.wind["speed"];
+            let date = isoToReadableDate(data.dt);
+            
+
+            currentWeatherElement.children[0].innerHTML += " " + date;
+            currentWeatherElement.children[1].setAttribute("src", "./Assets/Images/icons/" + iconCode + ".png");
+            currentWeatherElement.children[2].innerHTML = "Temp: " + temp + "°C";
+            currentWeatherElement.children[3].innerHTML = "Humidity: " + humidity + "%";
+            currentWeatherElement.children[4].innerHTML = "Wind Speed: " + windSpeed + "m/s";
         })
     }
 
-// Create a function to display the weather data on the page
+// Create a function to display the forecast weather data on the page
+function getForecastWeather () {
+    let lat = currentWeatherElement.getAttribute('data-lat');
+    let lon = currentWeatherElement.getAttribute('data-lon');
+    let queryString = "https://pro.openweathermap.org/data/2.5/forecast/climate?lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
 
+    fetch(queryString)
+        .then( function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+        
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+        })
+    
+}
 
 // Create a function to handle the button click handlers
 function buttonClickHandler (event) {
@@ -112,6 +147,8 @@ function buttonClickHandler (event) {
         // Use the latitude and longited to get the weather --> getWeather(lat, long);
     }
 }
+
+init();
 
 // Make an event listener for the button that calls the getWeather function
 asideElement.addEventListener('click', buttonClickHandler);
