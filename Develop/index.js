dd0f32a3994186cb7fc2380762900d146d95dae8
John Abou <abounassar.john@gmail.com>
2023-01-28 21:38:07 -0500

    Added semantic HTML tags to the index.html file. Added a getCoordinates function to run in the search event listener. Also added a getCurrentWeather function to run in the search event listener.

--- Develop/index.js
+++ Develop/index.js
@@ -1,43 +1,96 @@
 // Define the global variables
-let lat;
-let long;
-let apiKey = '5d82752b5eec77e02284baee59150776';
+let APIkey = '5d82752b5eec77e02284baee59150776';
 let searchHistory = [];
 
-// Create a function to save the search history to local storage
+// Define the DOM variables
+let currentWeatherElement = document.querySelector('#current-weather');
 
+// Create a function to save the search history to local storage
+function saveSearchHistory() {
+    let currentSearch = document.querySelector('input').value();
+    searchHistory.push(currentSearch);
+    localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
+}
 
 // Create a function to get the search history from local storage and update the preset buttons
+function getSearchHistory() {
+    searchHistory = JSON.parse(localStorage.getItem('searchHistory'));
+    if (searchHistory) {
+        for (let i = 0; i < searchHistory.length; i++) {
+            document.querySelectorAll('.preset-button')[i].textContent = searchHistory[i];
+        }
+    }
+}
 
 // Create a function to get the latitude and longitude from the OpenWeather API
+function getCoordinates( cityName ) {
+    let queryString = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&appid=' + APIkey;
 
-// Create a function to get the weather from the OpenWeather API
-const getWeather = async (lat, long) => {
-    const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=imperial`
-    );
-    const weatherData = await response.json();
-    return weatherData;
+    fetch(queryString)
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+
+            // return the response as a JSON object
+            return response.json();
+        })
+        .then(function returnCoords (data) {
+
+            // Get the latitude and longitude from the response.
+            let latitude = data[0]['lat'];
+            let longitude = data[0]['lon'];
+            currentWeatherElement.setAttribute('data-lat', latitude);
+            currentWeatherElement.setAttribute('data-lon', longitude);
+        })        
+}
+
+//Create a function to get the current weather from the OpenWeather API
+function getCurrentWeather (lat, long) {
+    let queryString = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + long + "&appid="+ APIkey;
+    
+    fetch(queryString)
+        .then(function (response) {
+            if (!response.ok) {
+                throw response.json();
+            }
+            console.log(response);
+            console.log('In the getCurrentWeather function');
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
     }
 
 // Create a function to display the weather data on the page
 
 
 // Create a function to handle the button click handlers
-function (event) {
+function buttonClickHandler (event) {
     let elementClicked  = event.target; // Get the element that was clicked
     
     // verify that the element clicked was a button
     if (elementClicked.matches('button')) {
-        
-        // Grab the city name from the button that was clicked.
         // For the search bar, grab the city name from the input field
         // When using the preset buttons, grab the city name from the button text
         if (elementClicked.matches('.search')) {
             let city = document.querySelector('input').value;
+            getCoordinates(city);
+            let latitude = currentWeatherElement.getAttribute('data-lat');
+            console.log(latitude);
+            let longitude = currentWeatherElement.getAttribute('data-lon');
+            getCurrentWeather( latitude, longitude );
+                // getFutureWeather(coordinates);
+            };
         } else {
             let city = elementClicked.textContent;
-        }
+            getCoordinates(city);
+            let latitude = currentWeatherElement.getAttribute('data-lat');
+            let longitude = currentWeatherElement.getAttribute('data-lon');
+            getCurrentWeather( latitude, longitude );
+                // getFutureWeather(coordinates);
+            };
 
         // Save the city name to local storage and update the preset-button text elements
 
@@ -46,9 +99,7 @@ function (event) {
     
     
         // Use the latitude and longited to get the weather --> getWeather(lat, long);
-    }
 }
-
 
 // Define the DOM variables used with event listeners
 let asideElement = document.querySelector('aside');
