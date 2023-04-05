f2275717b1ede42075e07224bb9001b05fb78d73
john-abou <abounassar.john@gmail.com>
2023-04-05 13:21:42 -0400

    Refactor: Scraping the old application and creating a React weather application.

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+// import CSS style sheets
+import './assets/css/comp-style.css';
+
+ReactDOM.render(<App />, document.getElementById("root"));
